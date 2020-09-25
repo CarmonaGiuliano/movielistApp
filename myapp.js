58382846bc7379f9a78618c184b070acfea4cc00
Giuliano Carmona <67776103+CarmonaGiuliano@users.noreply.github.com>
2020-09-25 14:14:32 -0300

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const routes = require('./routes/MovieListRoutes');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./services/passport-setup');
